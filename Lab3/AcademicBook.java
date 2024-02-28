c4b168309f436b1d5400aa8a94b78c73d92edb64
Deema <90179257+DeemaSWE@users.noreply.github.com>
2024-02-28 11:53:38 +0300

    Update AcademicBook.java

--- Lab3/AcademicBook.java
+++ Lab3/AcademicBook.java
@@ -8,6 +8,14 @@ class AcademicBook extends Book {
         this.subject = subject;
     }
 
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
     @Override
     public String getMediaType() {
         if (isBestseller()) {
